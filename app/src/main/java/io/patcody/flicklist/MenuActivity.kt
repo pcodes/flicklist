65eb7a8b514c2c31cc91f59324197ba89b141607
Pat Cody <patcody028@gmail.com>
2019-11-18 13:27:02 -0500

    Added a bottom navigation bar

--- app/src/main/java/io/patcody/flicklist/MenuActivity.kt
+++ app/src/main/java/io/patcody/flicklist/MenuActivity.kt
@@ -2,11 +2,18 @@ package io.patcody.flicklist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MenuActivity : AppCompatActivity() {
 
+    lateinit var toolbar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavView)
     }
 }
