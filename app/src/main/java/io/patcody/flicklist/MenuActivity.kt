c703cd957bfc4e437af841dbf2cf40b67651258d
Pat Cody <patcody028@gmail.com>
2019-11-18 14:08:38 -0500

    Created two fragments for nav bar

--- app/src/main/java/io/patcody/flicklist/MenuActivity.kt
+++ app/src/main/java/io/patcody/flicklist/MenuActivity.kt
@@ -3,11 +3,39 @@ package io.patcody.flicklist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MenuActivity : AppCompatActivity() {
 
     lateinit var toolbar: ActionBar
+    
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.navigation_search -> {
+                toolbar.title = "Movie Search"
+                val searchFragment = SearchFragment.newInstance()
+                openFragment(searchFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigation_list -> {
+                toolbar.title = "Movie List"
+                val listFragment = ListFragment.newInstance()
+                openFragment(listFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +43,12 @@ class MenuActivity : AppCompatActivity() {
 
         toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        toolbar.title = "Movie Search"
+        val searchFragment = SearchFragment.newInstance()
+        openFragment(searchFragment)
+
+        
+        
     }
 }
